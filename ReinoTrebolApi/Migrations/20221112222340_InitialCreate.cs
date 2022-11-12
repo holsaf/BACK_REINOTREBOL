0c82d05d9bb3cca95689362165e47cf8ec5f5ce4
holsaf <87883786+holsaf@users.noreply.github.com>
2022-11-12 17:24:55 -0500

    migrations, succed compiled without Swagger

---
+++ ReinoTrebolApi/Migrations/20221112222340_InitialCreate.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReinoTrebolApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "ReinoTrebol");
+
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Solicitud",
+                schema: "ReinoTrebol",
+                columns: table => new
+                {
+                    IdSolicitud = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Nombre = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Apellido = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Identificacion = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Edad = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Estado = table.Column<int>(type: "int", nullable: false),
+                    AfinidadMagica = table.Column<int>(type: "int", nullable: false),
+                    Grimorio = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Solicitud", x => x.IdSolicitud);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Solicitud_Identificacion",
+                schema: "ReinoTrebol",
+                table: "Solicitud",
+                column: "Identificacion",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Solicitud",
+                schema: "ReinoTrebol");
+        }
+    }
+}
