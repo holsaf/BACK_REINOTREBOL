98dfc58bd08c7d6e35d0c771561de10a4b814cac
holsaf <87883786+holsaf@users.noreply.github.com>
2022-11-13 10:09:10 -0500

    Working, with response with enums problem

--- ReinoTrebolApi/Program.cs
+++ ReinoTrebolApi/Program.cs
@@ -1,5 +1,7 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.OpenApi.Models;
 using ReinoTrebolApi.Extensiones;
 using ReinoTrebolApi.Formatters;
 
@@ -10,7 +12,16 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var info = new OpenApiInfo
+    {
+        Title = "Reino del Trebol",
+        Version = "v1",
+        Description = "Provide access to Reino del Trebol API",
+    };
+    options.SwaggerDoc("v1", info);
+});
 
 builder.Services.AddControllers()
     .AddNewtonsoftJson();
@@ -21,18 +32,18 @@ builder.Services.AddReinoTrebolDbContext(builder.Configuration.GetConnectionStri
 // Add config of scoped services and Mappers.
 builder.Services.AddServices();
 
-builder.Services.AddMvc(options =>
-{
-    options.EnableEndpointRouting = false;
-    options.ReturnHttpNotAcceptable = true;
-    options.InputFormatters.Insert(0, JsonPatchInputFormatter.GetJsonPatchInputFormatter());
-}).AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
-    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-    options.JsonSerializerOptions.WriteIndented = true;
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-});
+//builder.Services.AddMvc(options =>
+//{
+//    options.EnableEndpointRouting = false;
+//    options.ReturnHttpNotAcceptable = true;
+//    options.InputFormatters.Insert(0, JsonPatchInputFormatter.GetJsonPatchInputFormatter());
+//}).AddJsonOptions(options =>
+//{
+//    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+//    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+//    options.JsonSerializerOptions.WriteIndented = true;
+//    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+//});
 
 var app = builder.Build();
 
